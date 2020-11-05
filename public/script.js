f9bd131f541367fc09a4cf9d95ce9191f939e291
Michael-Dougherty17 <mdough28@terpmail.umd.edu>
2020-11-05 17:56:54 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,32 +1,33 @@
 //Load PG county json
-const test = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'
+const test = "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json";
 
 //Empty restaurants array
-const restaurants = []
+const restaurants = [];
 
 //Fill array with PG county json
 fetch(test)
-    .then(blob => blob.json())
-    .then(data => restaurants.push(...data))
+  .then((blob) => blob.json())
+  .then((data) => restaurants.push(...data));
 
 //Match user inputs
 function findMatch(wordToMatch, restaurants) {
-    return restaurants.filter(business => {
-        const regex = new RegExp(wordToMatch, 'gi');
-        return business.name.match(regex) || business.category.match(regex)
-    });
+  return restaurants.filter((business) => {
+    const regex = new RegExp(wordToMatch, "gi");
+    return business.name.match(regex) || business.category.match(regex);
+  });
 }
 
 //Show matches between input and restaurants array
 function showMatch() {
-    const match = findMatch(this.value, restaurants);
-    const html = match.map(restaurants => {
-        return `
+  const match = findMatch(this.value, restaurants);
+  const html = match
+    .map((restaurants) => {
+      return `
         <li>
             <ul>
                 <h2>${restaurants.name}</h2>
+                <li>${restaurants.category}</li>
                 <address>
-                    ${restaurants.category}<br>
                     ${restaurants.address_line_1}<br>
                     ${restaurants.address_line_2}<br>
                     ${restaurants.city}<br>
@@ -35,18 +36,19 @@ function showMatch() {
         </ul>
         </li>
         `;
-    }).join('');
-    
-    results.innerHTML = html;
+    })
+    .join("");
 
-    //Remove blank second address lines
-    const regex2 = new RegExp("------<br>", 'gi');
-    editHtml = html.replace(regex2, "")
-    results.innerHTML = editHtml;
+  results.innerHTML = html;
+
+  //Remove blank second address lines
+  const regex2 = new RegExp("------<br>", "gi");
+  editHtml = html.replace(regex2, "");
+  results.innerHTML = editHtml;
 }
 
-const search = document.querySelector('.inputText');
-const results = document.querySelector('.target');
+const search = document.querySelector(".inputText");
+const results = document.querySelector(".target");
 
-search.addEventListener('keyup', showMatch);
-search.addEventListener('change', showMatch);
+search.addEventListener("keyup", showMatch);
+search.addEventListener("change", showMatch);
