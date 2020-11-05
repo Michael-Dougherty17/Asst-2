e1a10f43bda23b3efb93a18f246c07a29398eca6
jqha <70602851+jqha@users.noreply.github.com>
2020-11-05 17:26:34 -0500

    Updated script.js and styles.css
    -Updated script.js to use <address> correctly
    -Updated ------ line replacement to take into account <br>
    -Removed italic from css.styles because of address use in script.js

--- public/script.js
+++ public/script.js
@@ -1,52 +1,46 @@
-//load PG county json
+//Load PG county json
 const test = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'
 
-//empty restaurants array
+//Empty restaurants array
 const restaurants = []
 
-//fill array with PG county json
+//Fill array with PG county json
 fetch(test)
     .then(blob => blob.json())
     .then(data => restaurants.push(...data))
 
-//match user inputs
-function findMatch(wordToMatch, restaurants){
+//Match user inputs
+function findMatch(wordToMatch, restaurants) {
     return restaurants.filter(business => {
         const regex = new RegExp(wordToMatch, 'gi');
         return business.name.match(regex) || business.category.match(regex)
     });
 }
 
-//show matches between input and restaurants array
-function showMatch(){
+//Show matches between input and restaurants array
+function showMatch() {
     const match = findMatch(this.value, restaurants);
     const html = match.map(restaurants => {
         return `
         <li>
             <ul>
-                <li>
-                    <span class="name">${restaurants.name}</span>
-                </li>
-                <li>
-                    <span class="category">${restaurants.category}</span>
-                </li>
-                <li>
-                    <span class="address">${restaurants.address_line_1}</span>
-                </li>
-                <li>
-                    <span class="address">${restaurants.address_line_2}</span>
-                </li>
-                <li>
-                    <span class="address">${restaurants.city}</span>
-                </li>     
-                <li>
-                    <span class="address">${restaurants.zip}</span>
-                </li>
+                <h2>${restaurants.name}</h2>
+                <address>
+                    ${restaurants.category}<br>
+                    ${restaurants.address_line_1}<br>
+                    ${restaurants.address_line_2}<br>
+                    ${restaurants.city}<br>
+                    ${restaurants.zip}<br>
+                </address>
         </ul>
         </li>
         `;
     }).join('');
     results.innerHTML = html;
+    //Remove blank second address lines
+    const regex2 = new RegExp("------<br>", 'gi');
+    editHtml = html.replace(regex2, "")
+    results.innerHTML = editHtml;
 }
 
 const search = document.querySelector('.inputText');
