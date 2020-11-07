1d48d8a627ece9811c5e2be6043de52a2226ad7f
Ujwal-Gupta7 <ujwalgupta99@gmail.com>
2020-11-07 13:25:27 -0500

    update

--- public/script.js
+++ public/script.js
@@ -1,62 +1,64 @@
-//load PG county json
-const test = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'
+//Load PG county json
+const test = "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json";
 
-//empty restaurants array
-const restaurants = []
+//Empty restaurants array
+const restaurants = [];
 
-//fill array with PG county json
+//Fill array with PG county json
 fetch(test)
-    .then(blob => blob.json())
-    .then(data => restaurants.push(...data))
+  .then((blob) => blob.json())
+  .then((data) => restaurants.push(...data));
 
-//match user inputs
-function findMatch(wordToMatch, restaurants){
-    return restaurants.filter(business => {
-        const regex = new RegExp(wordToMatch, 'gi');
-        return business.name.match(regex) || business.category.match(regex)
-    });
+//Match user inputs
+function findMatch(wordToMatch, restaurants) {
+  return restaurants.filter((business) => {
+    const regex = new RegExp(wordToMatch, "gi");
+    return business.name.match(regex) || business.category.match(regex);
+  });
 }
 
-//show matches between input and restaurants array
-function showMatch(){
-    const match = findMatch(this.value, restaurants);
-    const html = match.map(restaurants => {
-        return `
+//Titlecase extra credit
+function titleCaseFunction(restaurantTitle){
+    splitRestaurant = restaurantTitle.toLowerCase();
+    splitRestaurant = splitRestaurant.split(' ');
+    for(i = 0; i < splitRestaurant.length; i++){
+        splitRestaurant[i] = splitRestaurant[i].charAt(0).toUpperCase() + splitRestaurant[i].substring(1);
+    }
+    return splitRestaurant.join(' ');
+} 
+
+//Show matches between input and restaurants array
+function showMatch() {
+  const match = findMatch(this.value, restaurants);
+  const html = match
+    .map((restaurants) => {
+      return `
         <li>
             <ul>
-                <li>
-                    <span class="name">${restaurants.name}</span>
-                </li>
-                <li>
-                    <span class="category">${restaurants.category}</span>
-                </li>
-                <li>
-                    <span class="address">${restaurants.address_line_1}</span>
-                </li>
-                <li>
-                    <span class="address">${restaurants.address_line_2}</span>
-                </li>
-                <li>
-                    <span class="address">${restaurants.city}</span>
-                </li>     
-                <li>
-                    <span class="address">${restaurants.zip}</span>
-                </li>
+                <h2>${titleCaseFunction(restaurants.name)}</h2>
+                <li>${titleCaseFunction(restaurants.category)}</li>
+                <address>
+                    ${titleCaseFunction(restaurants.address_line_1)}<br>
+                    ${titleCaseFunction(restaurants.address_line_2)}<br>
+                    ${titleCaseFunction(restaurants.city)}<br>
+                    ${restaurants.zip}<br>
+                </address>
         </ul>
         </li>
         `;
-    }).join('');
+    })
+    .join("");
 
-    const regex2 = new RegExp("------", 'gi');
-    editHtml = html.replace(regex2, "")
-    results.innerHTML = editHtml;
+  results.innerHTML = html;
 
-    results.innerHTML = html;
-
+  //Remove blank second address lines
+  const regex2 = new RegExp("------<br>", "gi");
+  editHtml = html.replace(regex2, "");
+  results.innerHTML = editHtml;
 }
 
-const search = document.querySelector('.inputText');
-const results = document.querySelector('.target');
+const search = document.querySelector(".inputText");
+const results = document.querySelector(".target");
 
-search.addEventListener('keyup', showMatch);
-search.addEventListener('change', showMatch);
+search.addEventListener("keyup", showMatch);
+search.addEventListener("change", showMatch);
