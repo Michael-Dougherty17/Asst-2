81e5ff3afa76d5394040834196f748dada6fd2d5
jqha <70602851+jqha@users.noreply.github.com>
2020-11-08 10:02:52 -0500

    Updated script.js
    -Renamed const test to const restaurantData for better context
    -Renamed parameter in titleCaseFunction from restaurantTitle to restaurantInfo for better context
    -Fixed issue of console errors caused by titleCaseFunction
    -No results displayed if the search bar is empty

--- public/script.js
+++ public/script.js
@@ -1,50 +1,68 @@
 //Load PG county json
-const test = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'
+const restaurantData = "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json";
 
 //Empty restaurants array
-const restaurants = []
+const restaurants = [];
 
 //Fill array with PG county json
-fetch(test)
-    .then(blob => blob.json())
-    .then(data => restaurants.push(...data))
+fetch(restaurantData)
+  .then((blob) => blob.json())
+  .then((data) => restaurants.push(...data));
 
 //Match user inputs
 function findMatch(wordToMatch, restaurants) {
-    return restaurants.filter(business => {
-        const regex = new RegExp(wordToMatch, 'gi');
-        return business.name.match(regex) || business.category.match(regex)
-    });
+  return restaurants.filter((business) => {
+    const regex = new RegExp(wordToMatch, "gi");
+    return business.name.match(regex) || business.category.match(regex);
+  });
 }
+
+//Titlecase extra credit
+function titleCaseFunction(restaurantInfo){
+    if(restaurantInfo != null) {
+        splitRestaurant = restaurantInfo.toLowerCase();
+        splitRestaurant = splitRestaurant.split(' ');
+        for(i = 0; i < splitRestaurant.length; i++){
+            splitRestaurant[i] = splitRestaurant[i].charAt(0).toUpperCase() + splitRestaurant[i].substring(1);
+        }
+        return splitRestaurant.join(' ');
+    }
+} 
 
 //Show matches between input and restaurants array
 function showMatch() {
-    const match = findMatch(this.value, restaurants);
-    const html = match.map(restaurants => {
-        return `
-        <li>
-            <ul>
-                <h2>${restaurants.name}</h2>
-                <address>
-                    ${restaurants.category}<br>
-                    ${restaurants.address_line_1}<br>
-                    ${restaurants.address_line_2}<br>
-                    ${restaurants.city}<br>
-                    ${restaurants.zip}<br>
-                </address>
-        </ul>
-        </li>
-        `;
-    }).join('');
-    results.innerHTML = html;
-    //Remove blank second address lines
-    const regex2 = new RegExp("------<br>", 'gi');
-    editHtml = html.replace(regex2, "")
-    results.innerHTML = editHtml;
+  const match = findMatch(this.value, restaurants);
+  const html = match
+    .map((restaurants) => {
+        if(this.value != ''){
+            return `
+                <li>
+                    <ul>
+                        <h2>${titleCaseFunction(restaurants.name)}</h2>
+                        <li>${titleCaseFunction(restaurants.category)}</li>
+                        <address>
+                            ${titleCaseFunction(restaurants.address_line_1)}<br>
+                            ${titleCaseFunction(restaurants.address_line_2)}<br>
+                            ${titleCaseFunction(restaurants.city)}<br>
+                            ${restaurants.zip}<br>
+                        </address>
+                </ul>
+                </li>
+                `;
+        }
+    })
+    .join("");
+
+  results.innerHTML = html;
+
+  //Remove blank second address lines
+  const regex2 = new RegExp("------<br>", "gi");
+  editHtml = html.replace(regex2, "");
+  results.innerHTML = editHtml;
 }
 
-const search = document.querySelector('.inputText');
-const results = document.querySelector('.target');
+const search = document.querySelector(".inputText");
+const results = document.querySelector(".target");
 
-search.addEventListener('keyup', showMatch);
-search.addEventListener('change', showMatch);
+search.addEventListener("keyup", showMatch);
+search.addEventListener("change", showMatch);
